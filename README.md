f82a682a461e7dd8047cc72875f90fefcae16724
mmiii <ivnivnch@gmail.com>
2015-03-18 20:56:38 +0200

    init
    init

---
+++ README.md
@@ -0,0 +1,174 @@
+foundation-apps-modal
+=====================
+
+Zurb [Foundation-apps](http://foundation.zurb.com/apps/) modal with angular promises.
+
+1. [Install](#install)
+2. [Usage](#usage)
+
+## Install
+
+Install with Bower:
+
+```
+bower install foundation-apps-modal
+```
+
+Reference the script:
+
+```html
+<script src="bower_components\foundation-apps-modal\foundation-apps-modal.js"></script>
+```
+
+Add the module zfaModal as a dependency to your application:
+
+```js
+var app = angular.module('myapp', ['zfaModal']);
+```
+
+## Usage
+
+#### Modal
+
+Custom modal:
+
+```js
+app.controller('Controller', function($scope, zfaModal) {
+  $scope.showModal = function() {
+  	zfaModal({
+        template: '<div zf-modal=""><a class="close-button" zf-close="">×</a><h4>Hello!</h4>' +
+            '<a ng-click="resolve()" class="button primary">OK/a><a ng-click="reject()" class="button secondary">Cancel</a></div>'
+    })
+        .then(function(value) { /* ... */ })
+        .catch(function() { /* ... */ });
+  };
+});
+```
+
+Modal with controller:
+
+```js
+app.controller('Controller', function($scope, zfaModal) {
+  $scope.showModal = function() {
+  	zfaModal({
+        controller: ['$scope', 'zfaModalDefer', function($scope, zfaModalDefer, message) {
+            $scope.message = message;
+        
+            $scope.ok = function() {
+                zfaModalDefer.resolve();
+            };
+            
+            $scope.cancel = function() {
+                zfaModalDefer.reject();
+            };
+        }],
+        template: '<div zf-modal="" class="tiny dialog"><h4>{{message}}</h4><a class="close-button" zf-close="">×</a>' +
+            '<a ng-click="ok()" class="button primary">OK/a><a ng-click="cancel()" class="button secondary">Cancel</a></div>',
+        locals: {
+            message: 'Hello!'
+        }
+    })
+        .then(function(value) { /* ... */ })
+        .catch(function() { /* ... */ });
+  };
+});
+```
+
+##### Modal options
+
+* `controller`: Modal controller.
+* `templateUrl`: Modal template url.
+* `template`: Modal template.
+* `locals`: Injection locals for modal controller.
+
+#### Modal provider
+
+Define the modal with the zfaModalProvider:
+
+```js
+app.config(['zfaModalProvider', function(zfaModalProvider) {
+  zfaModalProvider('myModal', {
+        controller: ['$scope', function($scope, message) {
+            $scope.message = message;
+        }],
+        template: '<div zf-modal="" class="tiny dialog"><h4>{{message}}</h4><a class="close-button" zf-close="">×</a>' +
+            '<a ng-click="resolve()" class="button primary">OK/a><a ng-click="reject()" class="button secondary">Cancel</a></div>',
+        locals: {
+            message: 'Hello!'
+        }
+  });
+}]);
+```
+
+Then call modal from controller:
+
+```js
+app.controller('Controller', function($scope, zfaModal) {
+  $scope.showModal = function() {
+  	zfaModal.myModal()
+        .then(function(value) { /* ... */ })
+        .catch(function() { /* ... */ });
+  };
+});
+```
+
+Overwrite locals:
+
+```js
+app.controller('Controller', function($scope, zfaModal) {
+  //overwrite locals value
+  $scope.showModal = function() {
+  	zfaModal.myModal({ message: "Bye!" })
+        .then(function(value) { /* ... */ })
+        .catch(function() { /* ... */ });
+  };
+});
+```
+
+##### Modal provider options
+
+* `controller`: Modal controller.
+* `templateUrl`: Modal template url.
+* `template`: Modal template.
+* `locals`: Injection locals for modal controller.
+
+#### Predefined modals
+
+##### Alert
+
+```js
+zfaModal.alert({ message: "Alert!!!" })
+    .then(function() { /* ... */ })
+    .catch(function() { /* ... */ });
+```
+
+locals:
+* `message`: Alert message.
+* `ok`: Ok button text.
+
+##### Confirm
+
+```js
+zfaModal.confirm({ message: "Are You ready?" })
+    .then(function() { /* ... */ })
+    .catch(function() { /* ... */ });
+```
+
+locals:
+* `message`: Confirm message.
+* `ok`: Ok button text.
+* `cancel`: Cancel button text.
+
+##### Prompt
+
+```js
+zfaModal.prompt({ message: "Type your text:" })
+    .then(function(value) { /* ... */ })
+    .catch(function() { /* ... */ });
+```
+
+locals:
+* `message`: Prompt message.
+* `value`: Input value.
+* `ok`: Ok button text.
+* `cancel`: Cancel button text.
---
+++ foundation-apps-modal.js
@@ -0,0 +1,172 @@
+(function() {
+    'use strict';
+    
+    angular.module('zfaModal', ['foundation'])
+        .provider('zfaModal', function() {
+            var configs = {};
+
+            function provider(id, config) {
+                if (typeof id != 'string') throw new Error("zfaModalProvider: id should be defined");
+                configs[id] = config;
+            }
+
+            provider.$get = function(FoundationApi, $controller, $rootScope, $http, $q, $compile, $timeout, $window, $templateCache) {
+                for (var id in configs) {
+                    if (!configs.hasOwnProperty(id)) continue;
+                    if (modalFactory[id]) throw new Error("zfaModal: forbidden id: " + id);
+
+                    modalFactory[id] = function(_config, locals) {
+                        var config = angular.extend({}, _config);
+                        config.locals = angular.extend({}, config.locals, locals);
+                        return modalFactory(config);
+                    }.bind(null, configs[id]);
+                }
+
+                return modalFactory;
+
+                function modalFactory(config) {
+                    config = angular.extend({}, config);
+                    if (!config.locals || typeof config.locals != 'object') config.locals = {};
+
+                    var defer = $q.defer();
+
+                    if(config.templateUrl) {
+                        if ($templateCache.get(config.templateUrl)) {
+                            defer.resolve($templateCache.get(config.templateUrl));
+                        } else {
+                            $http.get(config.templateUrl, {
+                                cache: $templateCache
+                            }).then(function(response) {
+                                defer.resolve(response.data);
+                            });
+                        }
+                    } else {
+                        defer.resolve(config.template || "");
+                    }
+
+                    return defer.promise
+                        .then(function(template) {
+                            var defer = $q.defer();
+
+                            var id = FoundationApi.generateUuid();
+
+                            var element = angular.element(template)
+                                .attr('id', id);
+
+                            var props = {
+                                'animationIn': 'animation-in',
+                                'animationOut': 'animation-out',
+                                'overlay': 'overlay',
+                                'overlayClose': 'overlay-close'
+                            };
+
+                            for (var prop in props) {
+                                if (!props.hasOwnProperty(prop)) continue;
+                                if(config[prop] !== undefined) element.attr(props[prop], config[prop]);
+                            }
+
+                            var container = angular.element(config.container || $window.document.body) ;
+                            container.append(element);
+
+                            var locals = angular.extend({}, config.locals);
+
+                            var scope;
+
+                            if (locals.$scope instanceof $rootScope.constructor) {
+                                scope = locals.$scope;
+                            } else {
+                                scope = $rootScope.$new();
+                            }
+
+                            scope.active = true;
+                            scope.resolve = defer.resolve.bind(defer);
+                            scope.reject = defer.reject.bind(defer);
+
+                            $compile(element)(scope);
+
+                            if (config.controller) {
+                                $controller(config.controller, angular.extend(locals, { $scope: scope, zfaModalDefer: defer }));
+                            }
+
+                            FoundationApi.publish(id, 'show');
+
+                            FoundationApi.subscribe(id, function(msg) {
+                                switch (msg) {
+                                    case 'toggle':
+                                        if (!scope.active) break;
+                                    case 'close':
+                                    case 'hide':
+                                        defer.reject(msg);
+
+                                        $timeout(function() {
+                                            if (config.locals && config.locals.scope) delete scope.active;
+                                            else scope.$destroy();
+                                            element.remove();
+                                        }, 3000);
+
+                                        FoundationApi.unsubscribe(id);
+
+                                        break;
+                                }
+                            });
+
+                            defer.promise.finally(function() {
+                                FoundationApi.publish(id, 'close');
+                                FoundationApi.unsubscribe(id);
+                            });
+
+                            return defer.promise;
+                        });
+                };
+            };
+
+            provider.$get.$inject = ['FoundationApi', '$controller', '$rootScope', '$http', '$q', '$compile', '$timeout', '$window', '$templateCache'];
+
+            return provider;
+        })
+        .config(['zfaModalProvider', function(zfaModalProvider) {
+            zfaModalProvider('alert', {
+                controller: ['$scope', 'ok', 'message', function($scope, ok, message) {
+                    $scope.ok = ok;
+                    $scope.message = message;
+                }],
+                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a></div>',
+                locals: {
+                    ok: "OK",
+                    message: ""
+                }
+            });
+        }])
+        .config(['zfaModalProvider', function(zfaModalProvider) {
+            zfaModalProvider('confirm', {
+                controller: ['$scope', 'ok', 'cancel', 'message', function($scope, ok, cancel, message) {
+                    $scope.ok = ok;
+                    $scope.cancel = cancel;
+                    $scope.message = message;
+                }],
+                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
+                locals: {
+                    ok: "OK",
+                    cancel: "Cancel",
+                    message: ""
+                }
+            });
+        }])
+        .config(['zfaModalProvider', function(zfaModalProvider) {
+            zfaModalProvider('prompt', {
+                controller: ['$scope', 'ok', 'cancel', 'message', function($scope, ok, cancel, message, value) {
+                    $scope.ok = ok;
+                    $scope.cancel = cancel;
+                    $scope.message = message;
+                    $scope.value = value;
+                }],
+                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><label><input type="text" ng-model="value"></label><a class="button primary" ng-click="resolve(value)">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
+                locals: {
+                    ok: "OK",
+                    cancel: "Cancel",
+                    message: "",
+                    value: ""
+                }
+            });
+        }]);
+})();
