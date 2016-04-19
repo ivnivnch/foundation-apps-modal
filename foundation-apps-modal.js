74e1c7924bee4829327fdf500f964157ec14f0b1
Nghi Le <vinhnghi223@gmail.com>
2016-04-19 15:57:38 +0300

    Updated sample modals in config

--- foundation-apps-modal.js
+++ foundation-apps-modal.js
@@ -127,48 +127,60 @@
             }
         })
         .config(['zfaModalProvider', function(zfaModalProvider) {
-            zfaModalProvider('alert', {
-                controller: ['$scope', 'ok', 'message', function($scope, ok, message) {
-                    $scope.ok = ok;
-                    $scope.message = message;
-                }],
-                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a></div>',
-                locals: {
-                    ok: "OK",
-                    message: ""
+
+            var modalSamples = [
+                {
+                    id:'alert',
+                    config: {
+                        controller: ['$scope', 'ok', 'message', function($scope, ok, message) {
+                            $scope.ok = ok;
+                            $scope.message = message;
+                        }],
+                        template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a></div>',
+                        locals: {
+                            ok: "OK",
+                            message: ""
+                        }
+                    }
+                },
+                {
+                    id:'confirm',
+                    config: {
+                        controller: ['$scope', 'ok', 'cancel', 'message', function($scope, ok, cancel, message) {
+                            $scope.ok = ok;
+                            $scope.cancel = cancel;
+                            $scope.message = message;
+                        }],
+                        template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
+                        locals: {
+                            ok: "OK",
+                            cancel: "Cancel",
+                            message: ""
+                        }
+                    }
+                },
+                {
+                    id:'prompt',
+                    config: {
+                        controller: ['$scope', 'ok', 'cancel', 'message', 'value', function($scope, ok, cancel, message, value) {
+                            $scope.ok = ok;
+                            $scope.cancel = cancel;
+                            $scope.message = message;
+                            $scope.value = value;
+                        }],
+                        template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><label><input type="text" ng-model="value"></label><a class="button primary" ng-click="resolve(value)">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
+                        locals: {
+                            ok: "OK",
+                            cancel: "Cancel",
+                            message: "",
+                            value: ""
+                        }
+                    }
                 }
-            });
-        }])
-        .config(['zfaModalProvider', function(zfaModalProvider) {
-            zfaModalProvider('confirm', {
-                controller: ['$scope', 'ok', 'cancel', 'message', function($scope, ok, cancel, message) {
-                    $scope.ok = ok;
-                    $scope.cancel = cancel;
-                    $scope.message = message;
-                }],
-                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><a class="button primary" ng-click="resolve()">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
-                locals: {
-                    ok: "OK",
-                    cancel: "Cancel",
-                    message: ""
-                }
-            });
-        }])
-        .config(['zfaModalProvider', function(zfaModalProvider) {
-            zfaModalProvider('prompt', {
-                controller: ['$scope', 'ok', 'cancel', 'message', 'value', function($scope, ok, cancel, message, value) {
-                    $scope.ok = ok;
-                    $scope.cancel = cancel;
-                    $scope.message = message;
-                    $scope.value = value;
-                }],
-                template: '<div zf-modal="" overlay="false" overlay-close="false" class="tiny dialog"><a class="close-button" zf-close="">×</a><h4>{{message}}</h4><label><input type="text" ng-model="value"></label><a class="button primary" ng-click="resolve(value)">{{ok}}</a><a class="button secondary" ng-click="reject()">{{cancel}}</a></div>',
-                locals: {
-                    ok: "OK",
-                    cancel: "Cancel",
-                    message: "",
-                    value: ""
-                }
-            });
+            ];
+
+            for(var i = 0;i<modalSamples.length;i++){
+                zfaModalProvider.register(modalSamples[i].id,modalSamples[i].config)
+            }
         }]);
 })();
