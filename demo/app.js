2617276d3fabbd4a0cc4a98a5baed7896105766c
Nghi Le <vinhnghi223@gmail.com>
2016-04-26 15:59:54 +0300

    Added basic demo app

---
+++ demo/app.js
@@ -0,0 +1,8 @@
+var demoApp = angular.module('demo', [
+    'zfaModal',
+    'foundation'
+]);
+
+demoApp.controller('demoCtrl', function () {
+    this.text='nghi';
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
+gulp.task('server', function() {
+    return gulp.src(['demo','node_modules','dist'])
+        .pipe($.webserver({
+            port: 8080,
+            livereload: true,
+            open:true
+        }));
+});
+
 gulp.task('build', function(){
     return gulp.src(['src/**/*.js'])
         .pipe($.angularFilesort())// With srcs.name + '.tpl.js' for the templates, this makes sure templates are included after other sources.
